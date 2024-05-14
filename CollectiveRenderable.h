92b2633bff3099dc72c01a4779ae6620df1029a5
goheungchoi <89227091+goheungchoi@users.noreply.github.com>
2024-05-14 21:29:17 +0900

    - Finished writing Rendering functionality in CollectiveRenderable and SingleRenderable. - Enable Sprite Justification and Alignment in CollectiveRenderable and SingleRenderable. - Added Text Writing function in CollectiveRenderable and SingleRenderable.

--- CollectiveRenderable.h
+++ CollectiveRenderable.h
@@ -2,51 +2,53 @@
 
 #include "IRenderable.h"
 
-enum H_DIRECTION {
-  LEFT    = 0b0001,
-  RIGHT   = 0b0010,
-  CENTER  = 0b0011,
-};
-
-enum V_DIRECTION { 
-  TOP     = 0b0100, 
-  CENTER  = 0b1100, 
-  BOTTOM  = 0b1000 
-};
-
 template <class T>
 class CollectiveRenderable : public IRenderable {
   static std::unordered_map<std::wstring, Bitmap*> _pSpriteRegister;
   static std::unordered_map<Bitmap*, CachedBitmap*> _pSpriteCacheData;
 
-  // static std::optional<CachedBitmap> _spriteCache;
-
  protected:
-  bool _caching;
-  Bitmap* _currentSprite;
-  std::wstring _currentTag;
+  // Cache Data
+  bool          _caching;
+  Bitmap*       _currentSprite;
+  CachedBitmap* _currentCachedBitmap;
+  Rect*         _currentSpriteRect;
+  std::wstring  _currentTag;
+
+  // Sprite Frames
   std::unordered_map<Bitmap*, Rect> _spriteRects;  // The destination Rect
 
-  // Pivot position
-  H_DIRECTION _horizontal;
-  V_DIRECTION _vertical;
+  // Pivot Position
+  H_DIRECTION _horizontal{H_DIRECTION::LEFT};
+  V_DIRECTION _vertical{V_DIRECTION::TOP};
 
   // Text Data
-  /*std::wstring _text;
-  PointF _textOrigin;
-  FontFamily _fontFamily;
-  Font _font;
-  StringFormat _textFormat;
-  SolidBrush _textBrush{Color(255, 0, 0, 0)};*/
+  std::wstring    _text;
+  PointF          _textPosition;
+  FontFamily      _fontFamily;
+  Font            _font;
+  StringFormat    _textFormat;  // Text alignment
+  SolidBrush      _textBrush;  // Color of the text
 
  public:
-  CollectiveRenderable() : _caching{false} {}
-  CollectiveRenderable(int x, int y, bool caching = false) 
-    : IRenderable(x, y), _caching{caching} {}
+  CollectiveRenderable(bool caching = false) : 
+    _caching{caching}, 
+    _currentSprite{nullptr}, 
+    _currentCachedBitmap{nullptr},
+    _currentSpriteRect{nullptr},
+
+    _textPosition(0.0f, 0.0f),
+    _fontFamily(L"Arial"), 
+    _font(&_fontFamily, 12, FontStyleRegular, UnitPixel),
+    _textBrush(Color(255, 0, 0, 0)) {}
+
+  CollectiveRenderable(int x, int y, bool caching = false)
+  : IRenderable(x, y), CollectiveRenderable(caching) {}
+  
   virtual ~CollectiveRenderable() {}
 
   /**
-   * @brief 스프라이트를 이 렌더러블 오브젝트에 바인딩 합니다.
+   * @brief 스프라이트를 태그와 함께 렌더러블 오브젝트에 바인딩 합니다.
    * @param pSprite 바인딩 할 스프라이트 포인터
    */
   void BindSpriteWithTag(Bitmap* pSprite, const std::wstring& tag) {
@@ -67,7 +69,8 @@ class CollectiveRenderable : public IRenderable {
   }
 
   /**
-   * @brief 스프라이트 프레임의 포지션을 변경된 피벗 포지션에 맞게 업데이트 합니다.
+   * @brief 스프라이트 프레임의 포지션을 변경된 피벗 포지션에 맞게 업데이트
+   * 합니다.
    * @param horizontal 수평 피벗 포지션
    * @param vertical 수직 피벗 포지션
    */
@@ -82,83 +85,160 @@ class CollectiveRenderable : public IRenderable {
   /**
    * @brief 이 오브젝트가 표시할 텍스트를 설정한다.
    */
-  /*void SetText(const wchar_t* text, float x, float y) {
-          _text.assign(text);
-          _textOrigin = {x, y};
-  }*/ // TODO: Proper handle of wstring!
+  void SetText(const wchar_t* text) { _text.assign(text); }
+  void SetTextPosition(float x, float y) { _textPosition = {x, y}; }
+
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return 상대적 위치의 X값
+   */
+  float GetTextPositionX() { return _textPosition.X; }
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return 상대적 위치의 Y값
+   */
+  float GetTextPositionY() { return _textPosition.Y; }
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return PointF 스트럭쳐
+   */
+  PointF GetTextPosition() { return _textPosition; }
+
+  /**
+   * @brief 폰트 패밀리를 설정합니다.
+   * @param fontFamily 폰트 패밀리, 예시) L"Arial"
+   */
+  void SetFontFamily(const char* fontFamily) {
+    _fontFamily = FontFamily(fontFamily);
+  }
 
   /**
    * @brief 텍스트의 폰트를 설정한다.
-   * @param fontFamily 폰트 패밀리, 예시) L"Arial"
    * @param fontSize 픽셀 단위 폰트 사이즈
    * @param fontStyle 폰트 스타일, 기본값 = Regular
    * @param unit 폰트 단위, 기본값 = Pixel
    */
-  // void SetFont(
-  //	const wchar_t* fontFamily,
-  //	int fontSize,
-  //	// TODO: 유저가 폰트 색깔을 지정할 수 있게 하기.
-  //	FontStyle fontStyle = FontStyleRegular,
-  //	Unit unit = UnitPixel
-  //) {
-  //	FontFamily fontFamily(fontFamily);
-  //	_font = Font(&fontFamily, fontSize, fontStyle, unit);
-  // }
-
-  /**
-   * @brief 폰트 정렬을 설정한다.
-   */
-  void SetTextAlignment() {}  // TODO:
-
-  /**
-   * @brief 
-   * @return 
-   */
-  Bitmap* GetSprite() { return _pSprite; }
-
-  void SetCurrentTag(const wchar_t* tag) {
-    _currentTag.assign(tag);
-    auto& it = _currentSprite[tag];
+  void SetFont(int fontSize, FontStyle fontStyle = FontStyleRegular,
+               Unit unit = UnitPixel) {
+    _font = Font(&_fontFamily, fontSize, fontStyle, unit);
   }
 
   /**
-   * @brief 
+   * @brief 텍스트의 색깔을 설정합니다. (0U ~ 255U)
+   * @param r 
    * @param g 
+   * @param b 
+   * @param a 
+   */
+  void SetFontColor(char r, char g, char b, char a = 255U) {
+    _textBrush.SetColor(Color(a, r, g, b));
+  }
+
+  /**
+   * @brief 폰트 수평 정렬을 설정한다.
+   * @param textHAlignmet LEFT, CENTER, or RIGHT
+   */
+  void SetTextHorizontalAlignment(H_DIRECTION textHAlignment) {
+    switch (textHAlignment) {
+      case H_DIRECTION::LEFT: {
+        _textFormat.SetLineAlignment(StringAlignmentNear);
+      } break;
+      case H_DIRECTION::CENTER: {
+        _textFormat.SetLineAlignment(StringAlignmentCenter);
+      } break;
+      case H_DIRECTION::RIGHT: {
+        _textFormat.SetLineAlignment(StringAlignmentFar);
+      } break;
+    }
+  } 
+
+  /**
+   * @brief 폰트 수직 정렬을 설정한다.
+   * @param textVJustification TOP, CENTER, or BOTTOM
+   */
+  void SetTextVerticalJustify(H_DIRECTION textVJustification) {
+    switch (textVJustification) {
+      case H_DIRECTION::TOP: {
+        _textFormat.SetAlignment(StringAlignmentNear);
+      } break;
+      case H_DIRECTION::CENTER: {
+        _textFormat.SetAlignment(StringAlignmentCenter);
+      } break;
+      case H_DIRECTION::BOTTOM: {
+        _textFormat.SetAlignment(StringAlignmentFar);
+      } break;
+    }
+  } 
+
+  /**
+   * @brief 현재 렌더링 하고 있는 스프라이트의 포인터를 리턴합니다.
+   * @return 스프라이트의 비트맵 포인터
+   */
+  Bitmap* GetCurrentSprite() { return _currentSprite; }
+
+  /**
+   * @brief 태그에 맞는 스프라이트로 전환합니다. 
+   * 이 함수는 다른 함수들에 비에 상대적으로 느릴 수 있습니다.
+   * @param tag 스프라이트 태그
+   */
+  void SetCurrentTag(const wchar_t* tag) {
+    _currentTag.assign(tag);
+    auto it = _pSpriteRegister.find(_currentTag);
+    if (it == _pSpriteRegister.end()) {
+      throw std::exception("CollectiveRenderer: Tag not found!");
+    }
+
+    //// Cache all necessary information for rendering.
+    // Set the current sprite
+    _currentSprite = it->second;
+    // Set the current cached bitmap
+    if (auto cacheIt = _pSpriteCacheData.find(_currentSprite); 
+        cacheIt == _pSpriteCacheData.end()) // Check if the cached sprite exists
+      _currentCachedBitmap = nullptr;  // If not, mark this sprite has no cache.
+    else
+      _currentCachedBitmap = cacheIt->second;
+    // Set the current frame
+    _currentSpriteRect = &_spriteRects[_currentSprite];
+  }
+
+  /**
+   * @brief
+   * @param g
    */
   void Render(Gdiplus::Graphics& g) override {
-    if (caching) {
-      
-    
+    if (_caching) {
+      // Check if the cached bitmap of the current sprite exists
+      if (!_currentCachedBitmap) {
+        // If not, create one.
+        _currentCachedBitmap = new CachedBitmap(_currentSprite, &g);
+        _pSpriteCacheData[_currentSprite] = _currentCachedBitmap;
+      }
+      g.DrawCachedBitmap(
+        _currentCachedBitmap, 
+        _currentSpriteRect->X,
+        _currentSpriteRect->Y
+      );
+    } else {
+      // If no caching
+      g.DrawImage(_currentSprite, *_currentSpriteRect);
     }
 
-
-        /*unsigned int x = _spriteRects[p.second].X;
-        unsigned int y = _spriteRects[p.second].Y;
-        g.DrawCachedBitmap(p.second, )*/
-
-
-    if (_pSprite) {
-      // if (!_spriteCache) {
-      //_spriteCache = CachedBitmap(_pSprite, &g);
-      //}
-
-      // g.DrawCachedBitmap(&(*_spriteCache), _position.X, _position.Y);
+    // Draw Text
+    if (!_text.empty()) {
+      g.DrawString(_text.c_str(), -1, &_font, _textPosition, &_textFormat, &_textBrush);
     }
-
-    // if (!_text.empty())
-    // g.DrawString(_text.c_str(), -1, &_font, _textOrigin, &_textFormat);
 
 #ifndef NDEBUG  // 디버깅일 때, 디버그용 사각형을 그림.
     Pen debuggingPen(Color::Red, 5.f);
-    g.DrawRectangle(&debuggingPen, _spriteRect);
+    g.DrawRectangle(&debuggingPen, *_currentSpriteRect);
 #endif
   }
 
  private:
   /**
    * @brief 피벗 포지션에 맞게 스프라이트 프레임을 조정합니다.
-   * @param sprite 
-   * @param spriteRect 
+   * @param sprite
+   * @param spriteRect
    */
   void __AdjustSpriteRect(Bitmap* sprite, Rect& spriteRect) {
     unsigned int width = sprite->GetWidth();
@@ -170,23 +250,23 @@ class CollectiveRenderable : public IRenderable {
     unsigned int frameY = _position.Y;
 
     switch (_horizontal) {
-      case LEFT: {
+      case H_DIRECTION::LEFT: {
       } break;
-      case CENTER: {
+      case H_DIRECTION::CENTER: {
         frameX -= halfW;
       } break;
-      case RIGHT: {
+      case H_DIRECTION::RIGHT: {
         frameX -= width;
       } break;
     }
 
     switch (_vertical) {
-      case TOP: {
+      case V_DIRECTION::TOP: {
       } break;
-      case CENTER: {
+      case V_DIRECTION::CENTER: {
         frameY -= halfH;
       } break;
-      case BOTTOM: {
+      case V_DIRECTION::BOTTOM: {
         frameY -= height;
       } break;
     }
--- SingleRenderable.h
+++ SingleRenderable.h
@@ -5,23 +5,39 @@
 template<class T>
 class SingleRenderable : public IRenderable {
 	static Bitmap* _pSprite;
-	//static std::optional<CachedBitmap> _spriteCache;
+  static CachedBitmap* _pSpriteCache;
 
 protected:
+	// Cache Data
+  bool _caching;
 	Rect _spriteRect;	// The destination Rect
-	/*std::wstring _text;
-	PointF _textOrigin;
-	Font _font;
-	StringFormat _textFormat;
-	SolidBrush _textBrush{Color(255, 0, 0, 0)};*/
+
+	// Pivot Position
+  H_DIRECTION _horizontal{H_DIRECTION::LEFT};
+  V_DIRECTION _vertical{V_DIRECTION::TOP};
+
+	// Text Data
+  std::wstring    _text;
+  PointF          _textPosition;
+  FontFamily      _fontFamily;
+  Font            _font;
+  StringFormat    _textFormat;  // Text alignment
+  SolidBrush      _textBrush;  // Color of the text
 
 
 public:
-	SingleRenderable() {};
-	SingleRenderable(int x, int y) 
-		: IRenderable(x, y), _spriteRect(x, y, 500, 500) {
-	}
-	virtual ~SingleRenderable() {};
+	SingleRenderable(bool caching = false) : 
+		_caching{caching},
+
+		_textPosition(0.0f, 0.0f),
+		_fontFamily(L"Arial"), 
+		_font(&_fontFamily, 12, FontStyleRegular, UnitPixel),
+		_textBrush(Color(255, 0, 0, 0)) {}
+
+	SingleRenderable(int x, int y, bool caching = false) 
+		: IRenderable(x, y), SingleRenderable(caching) {}
+
+	virtual ~SingleRenderable() {}
 
 	/**
 	* @brief 스프라이트를 이 렌더러블 오브젝트에 바인딩한다.
@@ -29,65 +45,196 @@ public:
 	*/
 	void BindSprite(Bitmap* pSprite) { 
 		_pSprite = pSprite; 
-		_spriteRect = Rect(	// Create the destination rect
-			_position.X, 
-			_position.Y, 
-			pSprite->GetWidth(), 
-			pSprite->GetHeight()
-		);
+
+		Rect spriteRect(  // Create the destination rect
+      _position.X, 
+      _position.Y, 
+      pSprite->GetWidth(), 
+      pSprite->GetHeight()
+    );
+
+		// Adjust the sprite Rect following pivot positions
+		__AdjustSpriteRect(pSprite, spriteRect);
+
+		// 스프라이트 프레임을 담습니다.
+		_spriteRect = std::move(spriteRect);
 	}
 
 	/**
-	* @brief 이 오브젝트가 표시할 텍스트를 설정한다.
-	*/
-	/*void SetText(const wchar_t* text, float x, float y) { 
-		_text.assign(text); 
-		_textOrigin = {x, y};
-	}*/ // TODO: Proper handle of wstring!
+   * @brief 스프라이트 프레임의 포지션을 변경된 피벗 포지션에 맞게 업데이트
+   * 합니다.
+   * @param horizontal 수평 피벗 포지션
+   * @param vertical 수직 피벗 포지션
+   */
+  void UpdateSpritePivotPosition(H_DIRECTION horizontal, V_DIRECTION vertical) {
+    _horizontal = horizontal;
+    _vertical = vertical;
+    __AdjustSpriteRect(_pSprite, _spriteRect);
 
-	/**
-	* @brief 텍스트의 폰트를 설정한다.
-	* @param fontFamily 폰트 패밀리, 예시) L"Arial"
-	* @param fontSize 픽셀 단위 폰트 사이즈 
-	* @param fontStyle 폰트 스타일, 기본값 = Regular
-	* @param unit 폰트 단위, 기본값 = Pixel
-	*/
-	//void SetFont(
-	//	const wchar_t* fontFamily, 
-	//	int fontSize, 
-	//	// TODO: 유저가 폰트 색깔을 지정할 수 있게 하기.
-	//	FontStyle fontStyle = FontStyleRegular,
-	//	Unit unit = UnitPixel
-	//) {
-	//	FontFamily fontFamily(fontFamily);
-	//	_font = Font(&fontFamily, fontSize, fontStyle, unit);
-	//}
+  }
+	
+  /**
+   * @brief 이 오브젝트가 표시할 텍스트를 설정한다.
+   */
+  void SetText(const wchar_t* text) { _text.assign(text); }
+  void SetTextPosition(float x, float y) { _textPosition = {x, y}; }
 
-	/**
-	* @brief 폰트 정렬을 설정한다.
-	*/
-	void SetTextAlignment() {}	// TODO:
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return 상대적 위치의 X값
+   */
+  float GetTextPositionX() { return _textPosition.X; }
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return 상대적 위치의 Y값
+   */
+  float GetTextPositionY() { return _textPosition.Y; }
+  /**
+   * @brief 텍스트의 스프라이트 프레임 안에서의 상대적 위치를 리턴한다.
+   * @return PointF 스트럭쳐
+   */
+  PointF GetTextPosition() { return _textPosition; }
+
+  /**
+   * @brief 폰트 패밀리를 설정합니다.
+   * @param fontFamily 폰트 패밀리, 예시) L"Arial"
+   */
+  void SetFontFamily(const char* fontFamily) {
+    _fontFamily = FontFamily(fontFamily);
+  }
+
+  /**
+   * @brief 텍스트의 폰트를 설정한다.
+   * @param fontSize 픽셀 단위 폰트 사이즈
+   * @param fontStyle 폰트 스타일, 기본값 = Regular
+   * @param unit 폰트 단위, 기본값 = Pixel
+   */
+  void SetFont(int fontSize, FontStyle fontStyle = FontStyleRegular,
+               Unit unit = UnitPixel) {
+    _font = Font(&_fontFamily, fontSize, fontStyle, unit);
+  }
+
+  /**
+   * @brief 텍스트의 색깔을 설정합니다. (0U ~ 255U)
+   * @param r 
+   * @param g 
+   * @param b 
+   * @param a 
+   */
+  void SetFontColor(char r, char g, char b, char a = 255U) {
+    _textBrush.SetColor(Color(a, r, g, b));
+  }
+
+  /**
+   * @brief 폰트 수평 정렬을 설정한다.
+   * @param textHAlignmet LEFT, CENTER, or RIGHT
+   */
+  void SetTextHorizontalAlignment(H_DIRECTION textHAlignment) {
+    switch (textHAlignment) {
+      case H_DIRECTION::LEFT: {
+        _textFormat.SetLineAlignment(StringAlignmentNear);
+      } break;
+      case H_DIRECTION::CENTER: {
+        _textFormat.SetLineAlignment(StringAlignmentCenter);
+      } break;
+      case H_DIRECTION::RIGHT: {
+        _textFormat.SetLineAlignment(StringAlignmentFar);
+      } break;
+    }
+  } 
+
+  /**
+   * @brief 폰트 수직 정렬을 설정한다.
+   * @param textVJustification TOP, CENTER, or BOTTOM
+   */
+  void SetTextVerticalJustify(H_DIRECTION textVJustification) {
+    switch (textVJustification) {
+      case H_DIRECTION::TOP: {
+        _textFormat.SetAlignment(StringAlignmentNear);
+      } break;
+      case H_DIRECTION::CENTER: {
+        _textFormat.SetAlignment(StringAlignmentCenter);
+      } break;
+      case H_DIRECTION::BOTTOM: {
+        _textFormat.SetAlignment(StringAlignmentFar);
+      } break;
+    }
+  } 
 
 	Bitmap* GetSprite() { return _pSprite; }
 
 	void Render(Gdiplus::Graphics& g) override {
-		if (_pSprite) {
-			//if (!_spriteCache) {
-				//_spriteCache = CachedBitmap(_pSprite, &g);
-			//}
-			
-			//g.DrawCachedBitmap(&(*_spriteCache), _position.X, _position.Y);
-		}
+    if (_caching) {
+      // Check if the cached bitmap of the current sprite exists
+      if (!_pSpriteCache) {
+        // If not, create one.
+        _pSpriteCache = new CachedBitmap(_pSprite, &g);
+      }
+      g.DrawCachedBitmap(
+        _pSpriteCache, 
+        _spriteRect->X,
+        _spriteRect->Y
+      );
+    } else {
+      // If no caching
+      g.DrawImage(_pSprite, _spriteRect);
+    }
 
-		//if (!_text.empty())
-			//g.DrawString(_text.c_str(), -1, &_font, _textOrigin, &_textFormat);
-		
+		// Draw Text
+    if (!_text.empty()) {
+      g.DrawString(_text.c_str(), -1, &_font, _textPosition, &_textFormat, &_textBrush);
+    }
+
 #ifndef NDEBUG	// 디버깅일 때, 디버그용 사각형을 그림.
 		Pen debuggingPen(Color::Red, 5.f);
 		g.DrawRectangle(&debuggingPen, _spriteRect);
 #endif
 	}
+
+private:
+	/**
+   * @brief 피벗 포지션에 맞게 스프라이트 프레임을 조정합니다.
+   * @param sprite
+   * @param spriteRect
+   */
+  void __AdjustSpriteRect(Bitmap* sprite, Rect& spriteRect) {
+    unsigned int width = sprite->GetWidth();
+    unsigned int height = sprite->GetHeight();
+    unsigned int halfW = width / 2;
+    unsigned int halfH = height / 2;
+
+    unsigned int frameX = _position.X;
+    unsigned int frameY = _position.Y;
+
+    switch (_horizontal) {
+      case H_DIRECTION::LEFT: {
+      } break;
+      case H_DIRECTION::CENTER: {
+        frameX -= halfW;
+      } break;
+      case H_DIRECTION::RIGHT: {
+        frameX -= width;
+      } break;
+    }
+
+    switch (_vertical) {
+      case V_DIRECTION::TOP: {
+      } break;
+      case V_DIRECTION::CENTER: {
+        frameY -= halfH;
+      } break;
+      case V_DIRECTION::BOTTOM: {
+        frameY -= height;
+      } break;
+    }
+
+    spriteRect.X = frameX;
+    spriteRect.Y = frameY;
+  }
 };
 
 template<class T>
 Bitmap* SingleRenderable<T>::_pSprite{ nullptr };
+
+template<class T>
+CachedBitmap* SingleRenderable<T>::_pSpriteCache{ nullptr };
