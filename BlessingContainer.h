dd24b791c177a7c84315d0595bccd06a2ef9ef8c
goheungchoi <89227091+goheungchoi@users.noreply.github.com>
2024-05-20 15:54:55 +0900

    tmp

--- BlessingContainer.h
+++ BlessingContainer.h
@@ -2,15 +2,35 @@
 
 #include "Container.h"
 
+using namespace Gdiplus;
 
+enum class BlesssingType {
+
+};
 
 class BlessingContainer : public Container {
-	Container thumbnail;
+	Bitmap* thumbnail;
 	Container title;
 	Container explanation;
 
 public:
-	Blessingcontainer()
+	BlessingContainer() {
+		AddChildComponent(&title);
+		Add
+	}
 
+
+	void Draw(Graphics& g) {
+		// Set Transform
+		g.MultiplyTransform(&_transform);
+
+
+
+
+		// Reset Transform
+		Matrix* mClone = _transform.Clone();
+		mClone->Invert();
+		g.MultiplyTransform(mClone);
+	}
 
 };
